479ef35b9a4d427a390fa19b4c8e2d4d58a6930e
elyobsemaj <54197717+elyobsemaj@users.noreply.github.com>
2020-08-29 21:53:21 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,19 +1,13 @@
 const gameState = {
-	score: 0
+	
 };
 
 const config = {
 	type: Phaser.AUTO,
-	width: 450,
-	height: 500,
+	parent: 'content'
+	width: 640,
+	height: 512,
 	backgroundColor: "b9eaff",
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 },
-			enableBody: true,
-		}
-	},
 	scene: [StartScene, GameScene]
 };
 
