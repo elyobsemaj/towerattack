015230fbeb55c40f0b874bc8821bb108635d4cd1
elyobsemaj <54197717+elyobsemaj@users.noreply.github.com>
2020-08-30 10:36:21 -0700

    Update StartScene.js

--- StartScene.js
+++ StartScene.js
@@ -6,7 +6,7 @@ class StartScene extends Phaser.Scene {
 	create() {
 		this.add.text( 150, 250, 'Click to start!', {fill: '#000000', fontSize: '20px'})
 		this.input.on('pointerdown', () => {
-			this.scene.stop('StarScene')
+			this.scene.stop('StartScene')
 			this.scene.start('GameScene')
 		})
 	}
